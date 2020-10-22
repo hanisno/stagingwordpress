d6e17e7edae9ae2cd2f8f219d63c34367db63b54
hanis <hanisno@outlook.com>
2020-10-22 20:12:11 +0800

    Update wp-config

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+define('WP_HOME','http://stagingwp.hanisnoor.com');
+define('WP_SITEURL','http://stagingwp.hanisnoor.com');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'accelerate' );
