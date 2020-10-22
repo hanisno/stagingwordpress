22eb38f0d29bcdc4e0d4e09c7c2a5acd32cb7883
hanis <hanisno@outlook.com>
2020-10-22 20:18:42 +0800

    Update wp-config

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@ define('WP_SITEURL','http://stagingwp.hanisnoor.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'accelerate' );
+define( 'DB_NAME', 'stagingwp' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'haniswp' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '' );
